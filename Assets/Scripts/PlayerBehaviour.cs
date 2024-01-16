b4380480999bb9e9970316973eb1a08aeed742a4
mjemiolek <2000mj@wp.pl>
2024-01-16 16:59:21 +0100

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public GameObject Player;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            Player.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player.transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
